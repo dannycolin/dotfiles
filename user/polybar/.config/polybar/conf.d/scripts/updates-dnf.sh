cce2c2a0fb15afbfa24bc3129c15af6a8c490c3b
Danny Colin <contact@dannycolin.com>
2021-03-17 22:35:20 -0400

    fix error handling
    Redirect autogenerated error message to /dev/null to avoid
    displaying it directly in the bar. Also, look if dnf returns
    an integer. If not, notice the user there was an error.

--- user/polybar/.config/polybar/conf.d/scripts/updates-dnf.sh
+++ user/polybar/.config/polybar/conf.d/scripts/updates-dnf.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-updates=$(dnf check-update -q | sed '/^\s*$/d' | wc -l)
+#Get updates
+updates=$(dnf list upgrades -q | wc -l)
 
-if [ "$updates" -gt 0 ]; then
+# Remove the extra empty line
+let "updates=$updates - 1"
+
+# Check if dnf returns an integer. If not, let the user know something went wrong.
+if expr "$updates" : '[0-9][0-9]*$'>/dev/null; then
+  if [ "$updates" -gt 0 ]; then
     echo "$updates"
+  fi
 else
-    echo ""
+  echo "Error"
 fi
+
