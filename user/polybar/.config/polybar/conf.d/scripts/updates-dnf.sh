a0e7ec5d22b54c7601103838c4613004593f5413
Danny Colin <contact@dannycolin.com>
2021-05-15 20:30:19 -0400

    update update-dnf module

--- user/polybar/.config/polybar/conf.d/scripts/updates-dnf.sh
+++ user/polybar/.config/polybar/conf.d/scripts/updates-dnf.sh
@@ -3,15 +3,15 @@
 #Get updates
 updates=$(dnf list upgrades -q | wc -l)
 
-# Remove the extra empty line
-let "updates=$updates - 1"
-
 # Check if dnf returns an integer. If not, let the user know something went wrong.
-if expr "$updates" : '[0-9][0-9]*$'>/dev/null; then
+if expr "$updates" : "[0-9]" > /dev/null; then
   if [ "$updates" -gt 0 ]; then
-    echo "$updates"
+    let "updates=$updates -1"
+    echo "U: $updates"
+  else
+    echo ""
   fi
 else
-  echo "Error"
+  echo "U: Error"
 fi
 
