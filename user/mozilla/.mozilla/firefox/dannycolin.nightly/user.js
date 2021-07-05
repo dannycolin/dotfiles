5797a83ed430efc628a97b1c621f2850facd4c9e
Danny Colin <contact@dannycolin.com>
2021-07-04 22:37:46 -0400

    update user.js
    - enable calculator and unit conversion in urlbar
    - disable search with banner
    - remove deprecated extensions.activeThemeID
    - add comments to document some prefs

--- user/mozilla/.mozilla/firefox/dannycolin.nightly/user.js
+++ user/mozilla/.mozilla/firefox/dannycolin.nightly/user.js
@@ -12,6 +12,7 @@ user_pref("browser.aboutConfig.showWarning", false);
 
 user_pref("browser.discovery.enabled", false);
 
+// Remove widgets on the newtab page
 user_pref("browser.newtabpage.activity-stream.default.sites", "");
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
@@ -21,24 +22,35 @@ user_pref("browser.tabs.drawInTitlebar", false);
 
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 
+// Enable compact mode
+//
 // browser.uidensity
 //
 //  0 - default
 //  1 - compact (deprecated with bug 1693028)
 //  2 - touch
+user_pref("browser.compactmode.show", true);
 user_pref("browser.uidensity", 1);
 
+// Show bookmarks first
 user_pref("browser.urlbar.showSearchSuggestionsFirst", false);
+
+// Don't show history or topsites
 user_pref("browser.urlbar.suggest.history", false);
 user_pref("browser.urlbar.suggest.topsites", false);
 
-user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
+// Disable the larger "search with" banner
+user_pref("browser.urlbar.tabToSearch.onboard.interactionsLeft", 0);
 
-// Load userChrome.css
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// Enable arthimetic expressions and unit conversions
+user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.urlbar.unitConversion.enabled", true);
 
 // Disable middle mouse click paste
 user_pref("middlemouse.paste", false);
+
+// Load userChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 // -----------------------------------------------------------------------------
 // Devtools
